2697424093474b1236704ea5057ea34bb526ba38
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2020-01-07 12:03:31 -0300

    :boom: Impleenting mongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 import databaseConfig from '../config/database';
 import User from '../app/models/user';
 import File from '../app/models/file';
@@ -8,6 +9,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -16,6 +18,13 @@ class Database {
     models
       .map((model) => model.init(this.connection))
       .map((model) => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true },
+    );
   }
 }
 
