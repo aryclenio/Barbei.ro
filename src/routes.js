85deec30f3b285bf1c5c33820f3f60cdcc16bddc
Aryclenio Xavier Barros <aryclenio2010@gmail.com>
2020-01-07 09:14:57 -0300

    :tada: Building proiders route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -14,5 +16,6 @@ routes.post('/sessions', SessionController.store);
 // O middleware só funcionará após as rotas de seção.
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 export default routes;
